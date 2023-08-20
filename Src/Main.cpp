334900a268ac996a01c3da0a938cfdec720a3f5d
L-proger <l-proger@yandex.ru>
2023-08-20 23:04:47 +0300

    *

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -1,7 +1,16 @@
 
-
-
+#include <filesystem>
+#include <iostream>
+#include <Windows.h>
 
 int main() {
+
+    
+    HMODULE lib = LoadLibraryA("Dependencies/MicroNetwork.Library/MicroNetwork.dll");
+
+    void* getInterfaceFunc = GetProcAddress(lib, "getInterface");
+
+    //extern "C" LFRAMEWORK_COM_LIBRARY_EXPORT void LFRAMEWORK_COM_LIBRARY_CALL getInterface(LFramework::ComPtr<LFramework::IUnknown>& result)
+    std::cout << std::filesystem::current_path() << std::endl;
     return 0;
 }
